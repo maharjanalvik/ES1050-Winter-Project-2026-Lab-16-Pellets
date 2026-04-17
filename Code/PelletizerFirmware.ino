c3b209dd13e7ef921800dbf56117a6a862fea824
maharjanalvik <maharjanalvik@gmail.com>
2026-04-17 06:37:57 -0400

    Update PelletizerFirmware.ino

--- Code/PelletizerFirmware.ino
+++ Code/PelletizerFirmware.ino
@@ -1,1 +1,24 @@
+#include "config.h"
+#include "ui.h"
+#include "motor.h"
+#include "input.h"
+#include "sensors.h"
+#include "state.h"
+#include "effects.h"
 
+void setup() {
+    initUI();
+    initMotor();
+    initInput();
+    initSensors();
+    initEffects();
+}
+
+void loop() {
+    readInput();
+    updateSensors();
+    updateState();
+    updateMotor();
+    updateUI();
+    updateEffects();
+}
